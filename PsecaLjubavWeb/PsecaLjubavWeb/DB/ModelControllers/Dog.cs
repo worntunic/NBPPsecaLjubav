3a72ec457abdfb063c0915582ffde36ad4edfe84
worntunic <tonic.lazar@gmail.com>
2020-08-24 18:07:11 +0200

    Added dog card partials, started work on home screen

---
+++ PsecaLjubavWeb/PsecaLjubavWeb/DB/ModelControllers/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsecaLjubavWeb.DB.Models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Sex { get; set; }
+        public string Race { get; set; }
+
+        public string GetDateAsReadableString()
+        {
+            //TODO: parse birthdate
+            return "3 meseca";
+        }
+        public string GetSexAsReadableString()
+        {
+            if (Sex == "Male")
+            {
+                return "Mužjak";
+            } else if (Sex == "Female")
+            {
+                return "Ženka";
+            }
+            return "Nepoznat pol";
+        }
+    }
+}
--- PsecaLjubavWeb/PsecaLjubavWeb/Pages/User/Home.cshtml.cs
+++ PsecaLjubavWeb/PsecaLjubavWeb/Pages/User/Home.cshtml.cs
@@ -11,18 +11,39 @@ namespace PsecaLjubavWeb.Pages.User
     public class HomeModel : UserPageModel
     {
         public string Username { get; set; }
+        public List<DB.Models.Dog> Dogs;
 
         private DB.Models.User user;
-
         public HomeModel(DB.Database database) : base(database)
         {
-            
+            FillDogList();
         }
 
         public void OnGet()
         {
             this.user = GetCurrentUser(HttpContext);
             this.Username = user.Username;
+        }
+
+        private void FillDogList()
+        {
+            Dogs = new List<DB.Models.Dog>();
+
+            DB.Models.Dog apa = new DB.Models.Dog();
+            apa.Name = "Apa";
+            apa.Image = "apa.jpg";
+            apa.Sex = "Female";
+            apa.Race = "Mešanac";
+            apa.BirthDate = DateTime.Parse("12.6.2020");
+            DB.Models.Dog pega = new DB.Models.Dog();
+            pega.Name = "Pega";
+            pega.Image = "pega.jpg";
+            pega.Sex = "Male";
+            pega.Race = "Mešanac";
+            pega.BirthDate = DateTime.Parse("12.6.2020");
+
+            Dogs.Add(apa);
+            Dogs.Add(pega);
         }
     }
 }
